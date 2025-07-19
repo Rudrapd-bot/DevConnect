2bd37338c0ac57e56eac362ed36806038f4182f0
Rudra Dubey <drudra021@gmail.com>
2025-07-19 11:48:20 +0530

    Deleted all files

--- CLLg/03/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
- console.log(a)
-//  a = 5;           
-// console.log(a);  
-// var a = 5;           
-// console.log(a);  
--- CLLg/14/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-let arr = [10 , 20 , 30 ,40 , 50 ,60];
-// let [a1,a2 ,...spread] = arr
-// console.log(spread);
